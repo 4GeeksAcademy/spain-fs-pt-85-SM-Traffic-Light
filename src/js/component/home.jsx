1491ed6a59da71aa8eb7b6b58f8149f13643ac1a
unduashe <unduashe@outlook.es>
2024-11-20 20:05:00 +0000

    generacion de semáforo con cambio de color de luz en click

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+// import {redColourHook, yellowColourHook, greenColourHook} from "..";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	// definición de los hooks
+	let [redColour, setRedColour] = useState("bg-light");
+    let [yellowColour, setYellowColour] = useState("bg-light");
+    let [greenColour, setGreenColour] = useState("bg-light");
+	// definición de las funciones para manejar los hooks
+    let redColourHandler = () => {
+        setRedColour("bg-danger");
+        setYellowColour("bg-light");
+        setGreenColour("bg-light");
+    }
+    let yellowColourHandler = () => {
+        setRedColour("bg-light");
+        setYellowColour("bg-warning")
+        setGreenColour("bg-light");
+    }
+    let greenColourHandler = () =>{
+        setRedColour("bg-light");
+        setYellowColour("bg-light");
+        setGreenColour("bg-success");
+    }
+    return (
+		<>
+			<div className="d-flex gap-5 justify-content-center">
+			<div className="d-flex bd-h-48 text-dark">
+				<div className="vr border-5 opacity-100"></div>
+			</div>
+			</div>
+			<div className=" col-1 mx-auto bg-dark p-3 rounded">
+				<div className={`col-1 rounded-circle p-5 m-3 mx-auto ${redColour}`} onClick={redColourHandler}></div>
+				<div className={`col-1 rounded-circle p-5 m-3 ${yellowColour} mx-auto`} onClick={yellowColourHandler}></div>
+				<div className={`col-1 rounded-circle p-5 m-3 ${greenColour} mx-auto`} onClick={greenColourHandler}></div>
+			</div>
+		</>
+    );
+}
 
 export default Home;
